f8a7ddaa0717831b1a813086e12b590e1fc6ca20
HaticeAkyol <akyolhatice61@gmail.com>
2019-12-17 10:42:05 -0600

    first commit

---
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -0,0 +1,15 @@
+package com.vytrack.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "/Users/hatice akyol/IdeaProjects/Summer2019OnlineCucumberSelenium/src/test/resources/feature"
+)
+public class CucumberRunner {
+}
+
+
+
