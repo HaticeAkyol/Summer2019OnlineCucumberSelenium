8c77ffe31b4068042241111ca8cee35c29530ec7
HaticeAkyol <akyolhatice61@gmail.com>
2020-01-02 09:47:05 -0600

    DDT with Scenario Outline

--- src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
@@ -54,6 +54,7 @@ public class LoginStepDefinitions {
     @Then("user enters {string} username and {string} password")
     public void user_enters_username_and_password(String string, String string2) {
         System.out.println("Login with "+string+" username and "+string2+" password.");
+        loginPage.login(string, string2);
     }
 
     @Then("user verifies that {string} message is displayed")
@@ -68,6 +69,16 @@ public class LoginStepDefinitions {
     public void user_logs_in_as_driver_with_following_credentials(Map<String, String> dataTable) {
         System.out.println(dataTable);
         loginPage.login(dataTable.get("username"), dataTable.get("password"));
+    }
+    @Then("user logs in as {string}")
+    public void user_logs_in_as(String role) {
+        loginPage.login(role);
+    }
+
+    @Then("the page title should be {string}")
+    public void the_page_title_should_be(String string) {
+        BrowserUtils.waitForPageTitle(string);
+        Assert.assertEquals("Title is incorrect", string, Driver.get().getTitle());
     }
 
 }
